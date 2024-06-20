97d72ff1c03b38867c3da6fecfd22a6ad5b5cc95
XadBo <Shao_Bo_x@163.com>
2024-06-20 17:39:13 +0800

    change Version

---
+++ models/RTD/BOT.py
@@ -0,0 +1,260 @@
+import torch
+import torch.nn as nn
+import math
+
+def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1, groups=1):
+    """standard convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
+                     padding=padding, dilation=dilation, groups=groups, bias=False)
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    # Pad to 'same' shape outputs
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+class Conv(nn.Module):
+    # Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        return self.act(self.conv(x))
+
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class Upsample(nn.Module):
+    def __init__(self, in_channels, out_channels, scale_factor=2):
+        super(Upsample, self).__init__()
+
+        self.upsample = nn.Sequential(
+            Conv(in_channels, out_channels, 1),
+            nn.Upsample(scale_factor=scale_factor, mode='bilinear')
+        )
+
+        # carafe
+        # from mmcv.ops import CARAFEPack
+        # self.upsample = nn.Sequential(
+        #     BasicConv(in_channels, out_channels, 1),
+        #     CARAFEPack(out_channels, scale_factor=scale_factor)
+        # )
+
+    def forward(self, x):
+        x = self.upsample(x)
+
+        return x
+
+
+class Downsample(nn.Module):
+    def __init__(self, in_channels, out_channels, scale_factor=2):
+        super(Downsample, self).__init__()
+
+        self.downsample = nn.Sequential(
+            Conv(in_channels, out_channels, scale_factor, scale_factor, 0)
+        )
+
+    def forward(self, x):
+        x = self.downsample(x)
+
+        return x
+
+class GSConv(nn.Module):
+    # GSConv https://github.com/AlanLi1997/slim-neck-by-gsconv
+    def __init__(self, c1, c2, k=1, s=1, g=1, act=True):
+        super().__init__()
+        c_ = c2 // 2
+        self.cv1 = Conv(c1, c_, k, s, None, g, act)
+        self.cv2 = Conv(c_, c_, 5, 1, None, c_, act)
+
+    def forward(self, x):
+        x1 = self.cv1(x)
+        x2 = torch.cat((x1, self.cv2(x1)), 1)
+        # shuffle
+        # y = x2.reshape(x2.shape[0], 2, x2.shape[1] // 2, x2.shape[2], x2.shape[3])
+        # y = y.permute(0, 2, 1, 3, 4)
+        # return y.reshape(y.shape[0], -1, y.shape[3], y.shape[4])
+
+        b, n, h, w = x2.data.size()
+        b_n = b * n // 2
+        y = x2.reshape(b_n, 2, h * w)
+        y = y.permute(1, 0, 2)
+        y = y.reshape(2, -1, n // 2, h, w)
+
+        return torch.cat((y[0], y[1]), 1)
+
+class MHSA(nn.Module):
+    def __init__(self, n_dims, width=14, height=14, heads=4, pos_emb=False):
+        super(MHSA, self).__init__()
+
+        self.heads = heads
+        self.query = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.key = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.value = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.pos = pos_emb
+        if self.pos:
+            self.rel_h_weight = nn.Parameter(torch.randn([1, heads, (n_dims) // heads, 1, int(height)]),
+                                             requires_grad=True)
+            self.rel_w_weight = nn.Parameter(torch.randn([1, heads, (n_dims) // heads, int(width), 1]),
+                                             requires_grad=True)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        n_batch, C, width, height = x.size()
+        q = self.query(x).view(n_batch, self.heads, C // self.heads, -1)
+        k = self.key(x).view(n_batch, self.heads, C // self.heads, -1)
+        v = self.value(x).view(n_batch, self.heads, C // self.heads, -1)
+        # print('q shape:{},k shape:{},v shape:{}'.format(q.shape,k.shape,v.shape))  #1,4,64,256
+        content_content = torch.matmul(q.permute(0, 1, 3, 2), k)  # 1,C,h*w,h*w
+        # print("qkT=",content_content.shape)
+        c1, c2, c3, c4 = content_content.size()
+        if self.pos:
+            # print("old content_content shape",content_content.shape) #1,4,256,256
+            content_position = (self.rel_h_weight + self.rel_w_weight).view(1, self.heads, C // self.heads, -1).permute(
+                0, 1, 3, 2)  # 1,4,1024,64
+
+            content_position = torch.matmul(content_position, q)  # ([1, 4, 1024, 256])
+            content_position = content_position if (
+                        content_content.shape == content_position.shape) else content_position[:, :, :c3, ]
+            assert (content_content.shape == content_position.shape)
+            # print('new pos222-> shape:',content_position.shape)
+            # print('new content222-> shape:',content_content.shape)
+            energy = content_content + content_position
+        else:
+            energy = content_content
+        attention = self.softmax(energy)
+        out = torch.matmul(v, attention.permute(0, 1, 3, 2))  # 1,4,256,64
+        out = out.view(n_batch, C, width, height)
+        return out
+
+
+class BottleneckTransformer(nn.Module):
+    # Transformer bottleneck
+    # expansion = 1
+
+    def __init__(self, c1, c2, stride=1, heads=4, mhsa=True, resolution=None, expansion=1):
+        super(BottleneckTransformer, self).__init__()
+        c_ = int(c2 * expansion)
+        self.cv1 = Conv(c1, c_, 1, 1)
+        # self.bn1 = nn.BatchNorm2d(c2)
+        if not mhsa:
+            self.cv2 = Conv(c_, c2, 3, 1)
+        else:
+            self.cv2 = nn.ModuleList()
+            self.cv2.append(MHSA(c2, width=int(resolution[0]), height=int(resolution[1]), heads=heads))
+            if stride == 2:
+                self.cv2.append(nn.AvgPool2d(2, 2))
+            self.cv2 = nn.Sequential(*self.cv2)
+        self.shortcut = c1 == c2
+        if stride != 1 or c1 != expansion * c2:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(c1, expansion * c2, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(expansion * c2)
+            )
+        self.fc1 = nn.Linear(c2, c2)
+
+    def forward(self, x):
+        out = x + self.cv2(self.cv1(x)) if self.shortcut else self.cv2(self.cv1(x))
+        return out
+
+
+class BoT3(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, e=0.5, e2=1, w=20, h=20):  # ch_in, ch_out, number,  expansion,w,h
+        super(BoT3, self).__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # act=FReLU(c2)
+        self.m = nn.Sequential(
+            *[BottleneckTransformer(c_, c_, stride=1, heads=4, mhsa=True, resolution=(w, h), expansion=e2) for _ in
+              range(n)])
+        # self.m = nn.Sequential(*[CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)])
+
+    def forward(self, x):
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
+
+
+
+class MultiHeadBOTT(nn.Module):
+    # Transformer bottleneck
+    # expansion = 1
+
+    def __init__(self, c1, c2, stride=1, heads=4, mhsa=True, resolution=None, expansion=1):
+        super(MultiHeadBOTT, self).__init__()
+        self.c = int(c2 * 0.5)
+        c_ = int(c2 * expansion)
+        self.cv1 = Conv(c1, c_, 1, 1)
+        # self.bn1 = nn.BatchNorm2d(c2)
+        if not mhsa:
+            self.cv2 = Conv(c_, c2, 3, 1)
+        else:
+            self.cv2 = nn.ModuleList()
+            self.cv2.append(MHSA(c2, width=int(resolution[0]), height=int(resolution[1]), heads=heads))
+            if stride == 2:
+                self.cv2.append(nn.AvgPool2d(2, 2))
+            self.cv2 = nn.Sequential(*self.cv2)
+        self.shortcut = c1 == c2
+        if stride != 1 or c1 != expansion * c2:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(c1, expansion * c2, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(expansion * c2)
+            )
+        self.fc1 = nn.Linear(c2, c2)
+
+    def forward(self, x):
+        N, C, H, W = x.size()
+        chanel = int(C*0.5)
+        x1, x2 = x.split((chanel, chanel), 1)
+        x1_out = self.cv2(self.cv1(x1))
+        x2_out = self.cv2(x1_out + self.cv1(x2))
+        x_out = torch.cat((x1_out, x2_out), dim=1)
+        # out = torch.cat((x,x_out), dim=1)
+        return x_out
+
+class BT2(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, e=0.5, e2=1, w=20, h=20):  # ch_in, ch_out, number, , expansion,w,h
+        super(BT2, self).__init__()
+        self.c = int(c2 * e)
+        c_ = int(c2 * e * 0.5)  # hidden channels
+        self.cv1 = Conv(c1, self.c, 1, 1)
+        self.cv2 = Conv(c1, self.c, 1, 1)
+        self.cv3 = Conv(2 * self.c, c2, 1)  # act=FReLU(c2)
+        self.bn = nn.BatchNorm2d(c2)
+        self.m = nn.Sequential(
+            *[MultiHeadBOTT(c_, c_, stride=1, heads=4, mhsa=True, resolution=(w, h), expansion=e2) for _ in
+              range(n)])
+
+
+    def forward(self, x):
+        x_in = self.cv1(x)
+        out1 = torch.cat((x_in, self.m(x_in)), dim=1)+self.bn(x)
+        out = self.cv3(out1)
+        return out
